d52c9bdcf5d47f56f98967ed669079993079ac28
Alexis ALVES <aalves@e3r13p11.42.fr>
2015-12-11 13:37:15 +0100

    added libft on the project

---
+++ libft/libft.h
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalves <aalves@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/24 10:47:29 by aalves            #+#    #+#             */
+/*   Updated: 2015/12/04 15:22:45 by aalves           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+
+typedef struct	s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}				t_list;
+
+void			ft_putchar(char c);
+void			ft_putchar_fd(char c, int fd);
+void			ft_putstr(char const *s);
+void			ft_putstr_fd(char const *s, int fd);
+void			ft_putendl(char const *s);
+void			ft_putendl_fd(char const *s, int fd);
+size_t			ft_strlen(char const *s);
+char			*ft_strcpy(char *dst, char const *src);
+char			*ft_strncpy(char *dst, char const *src, size_t n);
+char			*ft_strcat(char *restrict s1, char const *restrict s2);
+size_t			ft_strlcat(char *dst, char const *src, size_t size);
+char			*ft_strncat(char *s1, char const *s2, size_t n);
+char			*ft_strdup(char const *str);
+void			ft_bzero(void *s, size_t n);
+void			*ft_memset(void *b, int c, size_t len);
+void			*ft_memcpy(void *dst, void const *src, size_t n);
+void			*ft_memccpy(void *dst, void const *src, int c, size_t n);
+void			*ft_memmove(void *dst, void const *src, size_t len);
+void			*ft_memchr(void const *s, int c, size_t n);
+int				ft_memcmp(const void *s1, const void *s2, size_t n);
+void			*ft_memalloc(size_t size);
+void			ft_memdel(void **ap);
+char			*ft_strnew(size_t size);
+void			ft_strdel(char **as);
+void			ft_strclr(char *s);
+char			*ft_strchr(char const *s, int c);
+char			*ft_strrchr(char const *s, int c);
+char			*ft_strstr(char const *s1, char const *s2);
+char			*ft_strnstr(char const *s1, char const *s2, size_t n);
+char			**ft_strsplit(char const *s, char c);
+int				ft_islower(int c);
+int				ft_isupper(int c);
+int				ft_isalpha(int c);
+int				ft_isdigit(int c);
+int				ft_isalnum(int c);
+int				ft_isascii(int c);
+int				ft_isprint(int c);
+int				ft_toupper(int c);
+int				ft_tolower(int c);
+int				ft_isspace(int c);
+int				ft_strequ(char const *s1, char const *s2);
+int				ft_strnequ(char const *s1, char const *s2, size_t n);
+int				ft_strcmp(char const *s1, char const *s2);
+int				ft_strncmp(char const *s1, char const *s2, size_t n);
+char			*ft_strsub(char const *s, unsigned int start, size_t len);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_strtrim(char const *s);
+int				ft_atoi(char const *str);
+char			*ft_itoa(int n);
+void			ft_putnbr(int n);
+void			ft_putnbr_fd(int n, int fd);
+void			ft_striter(char *s, void (*f)(char *));
+void			ft_striteri(char *s, void (*f)(unsigned int, char *));
+char			*ft_strmap(char const *s, char (*f)(char));
+char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+t_list			*ft_lstnew(void const *content, size_t content_size);
+void			ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void			ft_lstadd(t_list **alst, t_list *new);
+void			ft_lstaddend(t_list *lst, t_list *new);
+void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list*));
+void			ft_print_memory(const void *addr, size_t size);
+
+#endif
