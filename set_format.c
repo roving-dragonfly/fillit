36d10d819c3753091f918b510eca87f7db3c5915
Alexis ALVES <aalves@e3r13p11.42.fr>
2015-12-11 19:50:02 +0100

    popipou

--- set_format.c
+++ set_format.c
@@ -6,34 +6,47 @@
 /*   By: aalves <aalves@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/11 19:16:45 by aalves            #+#    #+#             */
-/*   Updated: 2015/12/11 19:32:20 by aalves           ###   ########.fr       */
+/*   Updated: 2015/12/11 19:49:29 by aalves           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "fillit.h"
-#include <stdio.h>
+
 /*
+	Reference list
+
+	0 - 3	barre verticale
+	4 - 7	barre horizontale
+	8 - 16	carre
+	17 - 28	L symetrie haut
 
 
 
 
 */
 
-static void
+static char	**put_in_piece(t_tetri *tetri, char *data, char x, char y)
+{
+
+	tetri->piece = (char **)malloc(sizeof(data));
+	ft_memcpy(tetri->piece, data, sizeof(data));
+	tetri->x = x;
+	tetri->y = y;
+}
 
 void	set_format(t_tetri *tetri, char ref)
 {
 
-	if (ref >= 0 && ref <= 3)
-	{
-		tetri->piece = (char **)malloc(sizeof("#\0#\0#\0#\0"));
-		ft_memcpy(tetri->piece, bar1, sizeof("#\0#\0#\0#\0"));
-	}
-	else if (ref >= 0 && ref <= 3)
-	{
-		tetri->piece = (char **)malloc(sizeof(bar2));
-		ft_memcpy(tetri->piece, bar1, sizeof(bar2));
-	}
+	if (ref <= 3)
+		put_in_piece(&(tetri->piece), "#\0#\0#\0#\0", 1, 4);
+	if (ref <= 7)
+		put_in_piece(&(tetri->piece), "####\0", 4, 1);
+	if (ref <= 16)
+		put_in_piece(&(tetri->piece), "##\0##\0", 2, 2);
+	if (ref <= 28)
+		put_in_piece(&(tetri->piece), "##\0#\0#\0", 2, 3);
+	
 
 
 
